382cddee8166130800d27248c8833705cc238587
eder1104 <ederyairrodriguezparra1231@gmail.com>
2026-04-27 12:40:24 -0500

    cambio variable socket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,8 +8,8 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    key: import.meta.env.VITE_REVERB_APP_KEY || 'empty_key',
+    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
